c62b2a2bf612177386d29cbf5b3aef4e5d75a3df
Jakub Galgonek <jakub.galgonek@uochb.cas.cz>
2018-06-23 07:38:02 +0200

    Remove the pg prefix from names of rdfbox I/O functions

--- extension/pgsparql--0.1.sql
+++ extension/pgsparql--0.1.sql
@@ -4,8 +4,8 @@
 
 CREATE TYPE "rdfbox";
 
-CREATE FUNCTION "rdfbox_input"(cstring) RETURNS "rdfbox"  AS 'MODULE_PATHNAME','pg_rdfbox_input' LANGUAGE C IMMUTABLE STRICT;
-CREATE FUNCTION "rdfbox_output"("rdfbox") RETURNS cstring AS 'MODULE_PATHNAME','pg_rdfbox_output' LANGUAGE C IMMUTABLE STRICT;
+CREATE FUNCTION "rdfbox_input"(cstring) RETURNS "rdfbox"  AS 'MODULE_PATHNAME' LANGUAGE C IMMUTABLE STRICT;
+CREATE FUNCTION "rdfbox_output"("rdfbox") RETURNS cstring AS 'MODULE_PATHNAME' LANGUAGE C IMMUTABLE STRICT;
 
 CREATE TYPE "rdfbox"
 (
