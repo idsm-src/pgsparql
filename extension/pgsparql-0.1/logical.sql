0b7bb8073ec59bcb0f655e5a059bbeb55a1d65ce
Jakub Galgonek <jakub.galgonek@uochb.cas.cz>
2018-07-31 13:35:01 +0200

    Use parameter numbers instead of parameter names in sql code

--- extension/pgsparql-0.1/logical.sql
+++ extension/pgsparql-0.1/logical.sql
@@ -1,10 +1,10 @@
 -- effective boolean value
-CREATE FUNCTION "ebv_short"(x int2) RETURNS bool AS $$ select x != 0; $$ LANGUAGE SQL IMMUTABLE STRICT;
-CREATE FUNCTION "ebv_int"(x int4) RETURNS bool AS $$ select x != 0; $$ LANGUAGE SQL IMMUTABLE STRICT;
-CREATE FUNCTION "ebv_long"(x int8) RETURNS bool AS $$ select x != 0; $$ LANGUAGE SQL IMMUTABLE STRICT;
-CREATE FUNCTION "ebv_float"(x float4) RETURNS bool AS $$ select x != 'NaN'::float4 and x != 0; $$ LANGUAGE SQL IMMUTABLE STRICT;
-CREATE FUNCTION "ebv_double"(x float8) RETURNS bool AS $$ select x != 'NaN'::float8 and x != 0; $$ LANGUAGE SQL IMMUTABLE STRICT;
-CREATE FUNCTION "ebv_integer"(x decimal) RETURNS bool AS $$ select x != 0; $$ LANGUAGE SQL IMMUTABLE STRICT;
-CREATE FUNCTION "ebv_decimal"(x decimal) RETURNS bool AS $$ select x != 0; $$ LANGUAGE SQL IMMUTABLE STRICT;
-CREATE FUNCTION "ebv_string"(x varchar) RETURNS bool AS $$ select octet_length(x) != 0; $$ LANGUAGE SQL IMMUTABLE STRICT;
+CREATE FUNCTION "ebv_short"(int2) RETURNS bool AS $$ select $1 != 0; $$ LANGUAGE SQL IMMUTABLE STRICT;
+CREATE FUNCTION "ebv_int"(int4) RETURNS bool AS $$ select $1 != 0; $$ LANGUAGE SQL IMMUTABLE STRICT;
+CREATE FUNCTION "ebv_long"(int8) RETURNS bool AS $$ select $1 != 0; $$ LANGUAGE SQL IMMUTABLE STRICT;
+CREATE FUNCTION "ebv_float"(float4) RETURNS bool AS $$ select $1 != 'NaN'::float4 and $1 != 0; $$ LANGUAGE SQL IMMUTABLE STRICT;
+CREATE FUNCTION "ebv_double"(float8) RETURNS bool AS $$ select $1 != 'NaN'::float8 and $1 != 0; $$ LANGUAGE SQL IMMUTABLE STRICT;
+CREATE FUNCTION "ebv_integer"(decimal) RETURNS bool AS $$ select $1 != 0; $$ LANGUAGE SQL IMMUTABLE STRICT;
+CREATE FUNCTION "ebv_decimal"(decimal) RETURNS bool AS $$ select $1 != 0; $$ LANGUAGE SQL IMMUTABLE STRICT;
+CREATE FUNCTION "ebv_string"(varchar) RETURNS bool AS $$ select octet_length($1) != 0; $$ LANGUAGE SQL IMMUTABLE STRICT;
 CREATE FUNCTION "ebv_rdfbox"(rdfbox) RETURNS bool AS 'MODULE_PATHNAME' LANGUAGE C IMMUTABLE STRICT;
